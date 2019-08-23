d06d8667395f438a28cd23b562a3cb1fb8424971
woodonggwon@kopo <52773012+1807100045@users.noreply.github.com>
2019-08-23 19:43:40 +0900

    Add files via upload

---
+++ get-test.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>PHP GET Test</title>
+</head>
+<body>
+<form method="GET">
+	<input type="text" name="blank">
+	<button>send</button>
+</form>
+
+<?php
+echo $_GET['blank'}];
+?>
+</body>
+</html>
